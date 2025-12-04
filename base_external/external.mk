d0a04905da15c35e3ccf6b35df796d42c35023d6
Sebastian Torres <seto2103@colorado.edu>
2025-12-03 22:35:22 -0800

    Assignment 5 Part 2

---
+++ base_external/external.mk
@@ -0,0 +1,1 @@
+include $(BR2_EXTERNAL_AESD_ASSIGNMENT_5_PATH)/package/aesd-assignments/aesd-assignments.mk
---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the buildroot directory relative to this script
+cd buildroot
+
+# Run make distclean to clean up all build artifacts and configurations
+make distclean
